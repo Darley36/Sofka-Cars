610a7a7ec0c81096db1eedd4997336afd34a6889
Darley36 <Darley36@users.noreply.github.com>
2021-07-08 00:31:53 -0500

    Agregacion de la primera consulta

--- PruebaJuegoDeCarros/Datos/DB.cs
+++ PruebaJuegoDeCarros/Datos/DB.cs
@@ -5,15 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
+using PruebaJuegoDeCarros.Clases;
 
 namespace PruebaJuegoDeCarros.Datos
 {
     class DB
     {
-        static string cadena = "server= localhost; database= NAME;" +
+        static string cadena = "server= localhost; database= JUEGOCARROS;" +
             "integrated security= true";
 
         SqlConnection con = new SqlConnection(cadena);
+
+        public List<string> consultaCarros()
+        {
+            List<string> lista = new List<string>();
+            con.Open();
+            string Query = "SELECT Marca FROM CARRO";
+            SqlCommand cmd = new SqlCommand(Query, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    lista.Add(dr.GetString(0));
+                }
+            }
+            con.Close();
+            return lista;
+        }
 
     }
 }
